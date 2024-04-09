1f815f55ad24c8318d18bb9e412a6f98227e7eee
alvaroaaalves <alvaro93alves@gmail.com>
2024-04-09 14:02:30 -0300

    final aula 1 modulo 2

--- 02 - Dominando Python e Suas Estruturas de Dados/Aula 00/operadores_associacao.py
+++ 02 - Dominando Python e Suas Estruturas de Dados/Aula 00/operadores_associacao.py
@@ -1,6 +1,7 @@
 frutas = ["limao", "uva"]
-curso = "Curso de python"
+curso = "Curso de Python"
 
 print("laranja" not in frutas)
 print("limao" in frutas)
+print("python" in curso)
 print("Python" in curso)
