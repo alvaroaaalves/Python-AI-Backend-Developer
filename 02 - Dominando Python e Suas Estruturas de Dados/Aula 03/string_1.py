696ded20078e162dad3272663c586d1003901b81
alvaroaaalves <alvaro93alves@gmail.com>
2024-04-09 15:30:22 -0300

    aula 2.1

--- 02 - Dominando Python e Suas Estruturas de Dados/Aula 03/string_1.py
+++ 02 - Dominando Python e Suas Estruturas de Dados/Aula 03/string_1.py
@@ -4,15 +4,21 @@ print(nome.upper())
 print(nome.lower())
 print(nome.title())
 
+print ('')
 texto = "  Olá mundo!    "
 
+print (texto)
+print(texto.strip())
 print(texto + ".")
 print(texto.strip() + ".")
 print(texto.rstrip() + ".")
 print(texto.lstrip() + ".")
 
+
+print ('')
 menu = "Python"
 
+print (menu)
 print("####" + menu + "####")
 print(menu.center(14))
 print(menu.center(14, "#"))
