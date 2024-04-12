bd6e0b7b3c50fb371b8b71d8307026456ab9a3ea
alvaroaaalves <alvaro93alves@gmail.com>
2024-04-12 20:51:56 -0300

    atualização do arquivo de classes bicicleta

--- 03 - Orientação a Objetos e Boas Práticas em Python/Aula 00/01_desafio_bicicletaria.py
+++ 03 - Orientação a Objetos e Boas Práticas em Python/Aula 00/01_desafio_bicicletaria.py
@@ -1,10 +1,14 @@
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor):
+    def __init__(self, cor, modelo, ano, valor, aro=18):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
-
+        self.aro = aro
+        
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+    
     def buzinar(self):
         print("Plim plim...")
 
@@ -15,16 +19,22 @@ class Bicicleta:
     def correr(self):
         print("Vrummmmm...")
 
-    def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+    def trocar_marcha(self, nro_marcha):
+        print ("Trocando marcha")
+
+        def trocar_marcha():
+            print("ok")
+            if (nro_marcha > _self.marcha):
+                print ("Marcha trocada")
+            else:
+                print ("Não foi possível trocar de marcha")
 
 
 b1 = Bicicleta("vermelha", "caloi", 2022, 600)
 b1.buzinar()
 b1.correr()
 b1.parar()
-print(b1.cor, b1.modelo, b1.ano, b1.valor)
+b1._trocar_marcha(19)
+print ('\n',b1)
 
-b2 = Bicicleta("verde", "monark", 2000, 189)
-print(b2)
-b2.correr()
+
